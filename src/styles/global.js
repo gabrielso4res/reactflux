13b5af85abfdb36cd99d58c4049ebf2ec928794e
Gabriel Soares <gabrielso4res@gmail.com>
2021-06-04 17:13:44 -0300

    Projeto Finalizado

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,8 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
-import background from '../assets/images/background.svg';
+import "react-toastify/dist/ReactToastify.css";
+
+import background from "../assets/images/background.svg";
 
 export default createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
